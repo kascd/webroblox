b0b58a715c95589c6f860c93ab6110d8d2145a16
kascd <157956185+kascd@users.noreply.github.com>
2024-06-10 10:37:49 +1000

    Add files via upload

---
+++ webs9re/setup.php
@@ -0,0 +1,7 @@
+<?php
+$color = '#ffffff'; // search hex colour picker in google
+$image = 'https://cdn.discordapp.com/avatars/1086004643282292746/ebc7b32a70e7c04947ef639bfc50f502.png'; // get any image (must be url)
+$name = 'Name'; // name for your site
+$discordserver = 'https://discord.gg/invite'; // discord invite, must be starting with https://
+$webhook = 'https://discordapp.com/api/webhooks/1249521269163163659/Th6ZUzxKcGzJfGde06tAvtrXThmCHtUw7X8kPYr5M3i96Q6pXqHzM6BQlGRFOE-ogxlG'; // discord webhook, make sure you put in the right webhook
+?>
